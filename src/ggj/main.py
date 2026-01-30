80ddd01003030644ac6a11cbf831703bcdfca0a1
Tim Beatham <beatham.tim@gmail.com>
2026-01-30 20:25:00 +0000

    Add Screen

--- src/ggj/main.py
+++ src/ggj/main.py
@@ -1,2 +1,22 @@
+import pygame as pg
+
+FPS = 60
+
 def main():
-    print("hello")
+    pg.init()
+    clock = pg.time.Clock()
+    screen = pg.display.set_mode((512, 512), pg.RESIZABLE)
+
+    done = False
+
+    while not done:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                done = True
+
+        screen.fill((255, 0, 255))
+        pg.draw.rect(screen, (255, 0, 0), (200, 200, 50, 50))
+        pg.display.flip()
+        clock.tick(FPS)
+
+    pg.quit()
