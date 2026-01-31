e7fd6c48de88fd1218e2597461833ab4df5e1045
Tim Beatham <beatham.tim@gmail.com>
2026-01-31 02:52:23 +0000

    Add pointmass to the game

--- src/ggj/game_object.py
+++ src/ggj/game_object.py
@@ -1,5 +1,58 @@
 from typing import Protocol
 import pygame as pg
+from pygame.math import Vector2
+from typing import cast
+
+
+class PointMass:
+    """
+    Represents a point mass that we can apploy physics to
+    """
+
+    _position: pg.Vector2
+    _velocity: pg.Vector2
+    _accumulative_force: pg.Vector2
+    _mass: float
+    # todo(tbeatham): drag instead?
+    _max_speed: float
+
+    def __init__(self, position: pg.Vector2):
+        self._position = position
+        # todo(tbeatham) paarameterise this as part of the point mass
+        self._mass = 10.0
+        self._accumulative_force = pg.Vector2(0, 0)
+        self._velocity = pg.Vector2(0, 0)
+        self._position = pg.Vector2(0, 0)
+        self._max_speed = float("inf")
+
+    def add_force(self, force: pg.Vector2):
+        self._accumulative_force += force
+
+    def integrate(self):
+        acceleration = self._accumulative_force / self._mass
+
+        prev_velocity = self._velocity.copy()
+        self._velocity += acceleration
+
+        # clamp the velocity if at max speed
+        if self._max_speed != float("inf") and self._velocity.magnitude() > abs(
+            self._max_speed
+        ):
+            self._velocity = prev_velocity
+
+        self._position += self._velocity
+        self._accumulative_force = pg.Vector2(0, 0)
+
+    @property
+    def position(self) -> Vector2:
+        return self._position
+
+    @property
+    def velocity(self) -> Vector2:
+        return self._velocity
+
+    def clamp_speed(self, speed: float):
+        self._max_speed = speed
 
 
 class GameObject(Protocol):
--- src/ggj/player.py
+++ src/ggj/player.py
@@ -1,13 +1,15 @@
 import pygame as pg
 from ggj.camera import camera
 from ggj.keys import key_manager, key_map
-from ggj.game_object import GameObject
+from ggj.game_object import GameObject, PointMass
 
 PLAYER_SIZE = 50
+MAX_SPEED = 5
 
 
 class Player(pg.sprite.Sprite, GameObject):
     _world_rect: pg.Rect
+    _point_mass: PointMass
     image: pg.Surface
 
     def __init__(self):
@@ -16,27 +18,36 @@ class Player(pg.sprite.Sprite, GameObject):
         self.image = pg.Surface((PLAYER_SIZE, PLAYER_SIZE))
         self.image.fill(pg.Color(255, 0, 0, 0))
         self.rect = self.image.get_rect()
-
-        # contains the world position
-        self._world_rect = pg.Rect(0, 0, PLAYER_SIZE, PLAYER_SIZE)
-
+        self._point_mass = PointMass(pg.Vector2(0, 0))
+        self._point_mass.clamp_speed(MAX_SPEED)
         self._populate_rect()
 
     def _populate_rect(self):
         # contains the actual dimension on the screen
-        self.rect = camera.get_screen_rect(self._world_rect)
+        self.rect = camera.get_screen_rect(self.get_world_rect())
 
     def update(self) -> None:
+        force_multiplier = 2
+        net_force = pg.Vector2(0, 0)
+
         if key_manager.is_key_down(key_map.player_down):
-            self._world_rect.y += 10
+            net_force += pg.Vector2(0, 1)
         if key_manager.is_key_down(key_map.player_up):
-            self._world_rect.y -= 10
+            net_force += pg.Vector2(0, -1)
         if key_manager.is_key_down(key_map.player_left):
-            self._world_rect.x -= 10
+            net_force += pg.Vector2(-1, 0)
         if key_manager.is_key_down(key_map.player_right):
-            self._world_rect.x += 10
+            net_force += pg.Vector2(1, 0)
+
+        self._point_mass.add_force(force_multiplier * net_force)
+        self._point_mass.integrate()
 
         self._populate_rect()
 
     def get_world_rect(self) -> pg.Rect:
-        return self._world_rect
+        return pg.Rect(
+            self._point_mass.position.x,
+            self._point_mass.position.y,
+            PLAYER_SIZE,
+            PLAYER_SIZE,
+        )
